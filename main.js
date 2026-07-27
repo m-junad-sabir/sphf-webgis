b6469a603462988b8ef751a2caf04bde8d09d596
Muhammad_Junaid <junaidsabir2848@gmail.com>
2026-07-28 04:17:49 +0500

    all shikarpur layers uploaded and showing in group layer method. pop not working

--- main.js
+++ main.js
@@ -45,8 +45,6 @@ const [
         definitionExpression: "COUNTRY = 'Pakistan'"
     });
 
-    map.addMany([boundaryLayer,districtsLayer]);
-
     // Add main Deh FeatureLayer for administrative hierarchy
     const dehFeatureLayer = new GeoJSONLayer({
         url: "https://www.arcgis.com/sharing/rest/content/items/8ea72b39982844f7873ad2d529c57bf0/data",
@@ -60,12 +58,192 @@ const [
             `
         }
     });
-    map.add(dehFeatureLayer);
 
+    // Adding Deh Child Layers 
+    const M1_Abdul_Gaffoor_Jatoi = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/5f2031c1340145b6b54b33449c5056ca/data",
+        title: "M1 Abdul Gaffoor Jatoi",
+        visible: true
+    });
+    const M1_Abdul_Karim_Jatoi = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/70b69adf88a247da875cbcc8663579e0/data",
+        title: "M1 Abdul Kariim Jatoi",
+        visible: true
+    });
+    const M1_Bangul_Jatoi = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/cdc73404a70949f29e9eb2f0a7e471e3/data",
+        title: "M1 Bangul Jatoi",
+        visible: true
+    });
+    const M1_Dadilo_Khan_Jatoi = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/99ff164c26af41e7ae859548c2a1fa80/data",
+        title: "M1 Dadilo Khan Jatoi",
+        visible: true
+    });
+    const M1_Qadir_Pur = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/70a5fee21d16449eac3102383eec6889/data",
+        title: "M1 Qadir Pur",
+        visible: true
+    });
+    const M2_Habib_Kot = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/65c1b90f9c7547ce8d27ff16fb8b2787/data",
+        title: "M2 Habib Kot",
+        visible: true
+    });
+    const M2_Qazi_Wahan = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/2ea0ac8d3a184eafa6a606b11dab21dc/data",
+        title: "M2 Qazi Wahan",
+        visible: true
+    });
+    const M3_Jaffer_Dasti = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/98ee98fd86714378bf197732d3db1ec7/data",
+        title: "M3 Jaffer Dasti",
+        visible: true
+    });
+    const M3_Muhammad_Aalam_Shar = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/8c3448c3ab7e460294c428136d60c347/data",
+        title: "M3 Muhammad Aalam Shar",
+        visible: true
+    });
+    const M4_Nandhi_Kal_Wari = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/d96a0fd3108f4e58b5454992efa4817f/data",
+        title: "M4 Nandhi Kal Wari",
+        visible: true
+    });
+    const M4_Teenda = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/764be13b9e5144e597bfce54cef6ac6e/data",
+        title: "M4 Teenda",
+        visible: true
+    });
+    const M5_Haji_Ali_Muhammad_Brohi = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/4e33dda4f48e4b17826c52c21c26560a/data",
+        title: "M5 Haji Ali Muhammad Brohi",
+        visible: true
+    });
+    const M5_Juma_Khan_Brohi = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/f0828f6021264ab280828d91a74e49fa/data",
+        title: "M5 Juma Khan Brohi",
+        visible: true
+    });
+    const M5_Loung_Labano = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/df7b8fa81a3c42e0a039adf013e29939/data",
+        title: "M5 Loung Labano",
+        visible: true
+    });
+    const M6_MilitaryForm_QalandarBux = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/9aa1b945e91e476994f9aff2d9d7de02/data",
+        title: "M6 Military Form QalandarBux",
+        visible: true
+    });
+    const M7_Mad_Khoso = new GeoJSONLayer({
+        url: "https://www.arcgis.com/sharing/rest/content/items/ca8d8744e4fb4075a1862ca517838ade/data",
+        title: "M7 Mad Khoso",
+        visible: true
+    });
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    const dehkotHabibMahar_M1 = new GroupLayer({
+        title: "Deh kot Habib Mahar - M1",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M1_Abdul_Gaffoor_Jatoi, M1_Abdul_Karim_Jatoi, M1_Bangul_Jatoi, M1_Dadilo_Khan_Jatoi]
+
+    });
+
+    const dehkotHabibMahar_M2 = new GroupLayer({
+        title: "Deh kot Habib Mahar - M2",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M2_Habib_Kot]
+    });
+
+    const dehkotHabibMahar_M3 = new GroupLayer({
+        title: "Deh kot Habib Mahar - M3",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M3_Muhammad_Aalam_Shar]
+    });
+
+    const dehkotHabibMahar_M4 = new GroupLayer({
+        title: "Deh kot Habib Mahar - M4",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M4_Teenda]
+    });
+
+    const dehkotHabibMahar_M5 = new GroupLayer({
+        title: "Deh kot Habib Mahar - M5",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M5_Haji_Ali_Muhammad_Brohi, M5_Juma_Khan_Brohi]
+    });
+
+    const dehqaziwahan_M1 = new GroupLayer({
+        title: "Deh Qazi Wahan - M1",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M1_Qadir_Pur]
+    });
+
+    const dehqaziwahan_M2 = new GroupLayer({
+        title: "Deh Qazi Wahan - M2",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M2_Qazi_Wahan]
+    });
+
+    const dehqaziwahan_M3 = new GroupLayer({
+        title: "Deh Qazi Wahan - M3",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M3_Jaffer_Dasti]
+    });
+
+    const dehqaziwahan_M4 = new GroupLayer({
+        title: "Deh Qazi Wahan - M4",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M4_Nandhi_Kal_Wari]
+    });
+
+    const dehqaziwahan_M5 = new GroupLayer({
+        title: "Deh Qazi Wahan - M5",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M5_Loung_Labano]
+    });
+
+    const dehqaziwahan_M6 = new GroupLayer({
+        title: "Deh Qazi Wahan - M6",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M6_MilitaryForm_QalandarBux]
+    });
+
+    const dehqaziwahan_M7 = new GroupLayer({
+        title: "Deh Qazi Wahan - M7",
+        visible: true,
+        visibilityMode: "independent",
+        layers: [M7_Mad_Khoso]
+    });
+
+    map.addMany([ boundaryLayer, districtsLayer, dehFeatureLayer,
+        dehkotHabibMahar_M1, dehkotHabibMahar_M2, dehkotHabibMahar_M3, dehkotHabibMahar_M4, dehkotHabibMahar_M5,
+        dehqaziwahan_M1, dehqaziwahan_M2, dehqaziwahan_M3, dehqaziwahan_M4, dehqaziwahan_M5, dehqaziwahan_M6, dehqaziwahan_M7]);
+
+
+        // =============================================================================
+        // MAP & VIEW
+        // =============================================================================
+        // const map = new Map({
+        //     basemap: "satellite",
+        //     layers: [
+        //         boundaryLayer, districtsLayer, dehFeatureLayer, M1_Abdul_Gaffoor_Jatoi
+        //     ]
+        // });
     // 2. Initialize the MapView centered over Sindh, Pakistan
     const view = new MapView({
         container: "map",
-        map: map,
+        map : map,
         center: [68.456299194, 26.440361376], // [Longitude, Latitude]
         zoom: 9,
         ui: {
@@ -161,6 +339,55 @@ const [
         <img src="images/nespaklogo.png" alt="NESPAK Logo" class="logo-nespak-map" onerror="this.style.display='none'">
     `;
     view.ui.add(watermarkNode, "bottom-right");
+
+    // Auto-popup functionality for dynamic layer popups
+    function formatFieldName(name) {
+        return name
+            .replace(/_/g, ' ')
+            .replace(/([a-z])([A-Z])/g, '$1 $2')
+            .replace(/\b\w/g, char => char.toUpperCase());
+    }
+
+    function enableAutoPopupsForGeoJSON(layers) {
+        const systemFields = [
+            "objectid", "fid", "globalid", 
+            "shape", "shape_length", "shape_area", 
+            "geometry", "type"
+        ];
+
+        layers.forEach(layer => {
+            if (layer.type === "geojson" || layer.type === "feature") {
+                layer.when(() => {
+                    if (!layer.fields || layer.fields.length === 0) return;
+
+                    const visibleFieldInfos = layer.fields
+                        .filter(field => !systemFields.includes(field.name.toLowerCase()))
+                        .map(field => {
+                            return {
+                                fieldName: field.name,
+                                label: (field.alias && field.alias !== field.name) 
+                                    ? field.alias 
+                                    : formatFieldName(field.name),
+                                visible: true
+                            };
+                        });
+
+                    layer.popupTemplate = new PopupTemplate({
+                        title: layer.title ? `${layer.title} Details` : "Feature Attributes",
+                        content: [{
+                            type: "fields",
+                            fieldInfos: visibleFieldInfos
+                        }]
+                    });
+                }).catch(err => {
+                    console.error(`Error loading fields for layer ${layer.title || layer.id}:`, err);
+                });
+            }
+        });
+    }
+
+    // Enable auto-popups for all layers
+    enableAutoPopupsForGeoJSON([districtsLayer, boundaryLayer, dehFeatureLayer]);
 
     // 6. Administrative Hierarchy with FeatureLayer Queries
     const districtSelect = document.getElementById('districtSelect');
